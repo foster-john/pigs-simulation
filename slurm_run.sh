00e6137f742d3e04d2ecc7126708e72d5ae1acd0
John.Foster <john.foster@aapksmanhpc01.cm.cluster>
2023-12-11 16:38:51 -0600

    slurm testing

--- slurm_run.sh
+++ slurm_run.sh
@@ -1,7 +1,11 @@
-#!usr/bin/env bash
+#!/usr/bin/env bash
 #SBATCH --ntasks=1                # number of processes to run
 #SBATCH --nodes=1                 # default number of nodes
 #SBATCH --partition=cpu_compute   # good enough for what I need
 #SBATCH --cpus-per-task=1         # for a multithredded job
 #SBATCH --job-name=test_sim       # job name
-#SBATCH --output=test_sim.log     # output file
+#SBATCH --output=test_sim.txt     # output file
+
+
+module load R
+Rscript R/workflow.R $SLURM_ARRAY_TASK_ID 
