75d7433c42d195f81fa6e3f182464509b8e3fac3
JohnRFoster <fosterj@bu.edu>
2023-12-11 13:53:25 -0700

    adding slurm job script

---
+++ slurm_run.sh
@@ -0,0 +1,7 @@
+#!usr/bin/env bash
+#SBATCH --ntasks=1                # number of processes to run
+#SBATCH --nodes=1                 # default number of nodes
+#SBATCH --partition=cpu_compute   # good enough for what I need
+#SBATCH --cpus-per-task=1         # for a multithredded job
+#SBATCH --job-name=test_sim       # job name
+#SBATCH --output=test_sim.log     # output file
