4f90dccef63217760292356f1388b0c7e72885db
John.Foster <john.foster@aapksmanhpc01.cm.cluster>
2023-12-11 18:29:07 -0600

    more arrays

--- slurm_run.sh
+++ slurm_run.sh
@@ -4,8 +4,8 @@
 #SBATCH --partition=cpu_compute   # good enough for what I need
 #SBATCH --cpus-per-task=1         # for a multithredded job
 #SBATCH --job-name=test_sim       # job name
-#SBATCH --output=test_sim%J.txt     # output file 
-#SBATCH --array=1-3
+#SBATCH --output=test_sim%J.txt   # output file 
+#SBATCH --array=1-30              # number of arrays
 
 module load R
 Rscript R/workflow.R $SLURM_ARRAY_TASK_ID 
