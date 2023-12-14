4ab4026c0a0dbac285d9fdd46867da688fc0c89a
John.Foster <john.foster@aapksmanhpc01.cm.cluster>
2023-12-14 16:57:01 -0600

    getting ready for parallel on hpc

--- slurm_run.sh
+++ slurm_run.sh
@@ -2,11 +2,10 @@
 #SBATCH --ntasks=1                # number of processes to run
 #SBATCH --nodes=1                 # default number of nodes
 #SBATCH --partition=cpu_compute   # good enough for what I need
-#SBATCH --cpus-per-task=1         # for a multithredded job
+#SBATCH --cpus-per-task=96        # for a multithredded job
 #SBATCH --job-name=test_sim       # job name
 #SBATCH --output=test_sim%J.txt   # output file 
-#SBATCH --array=1-30              # number of arrays
-
 
 module load R
-Rscript R/workflow.R $SLURM_ARRAY_TASK_ID 
+Rscript R/workflow.R 
+
