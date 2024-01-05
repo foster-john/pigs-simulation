689fa90d210eac61b957a77d2911f99e9d7d7151
JohnRFoster <john.foster@usda.gov>
2024-01-05 13:27:45 -0600

    slurm

--- slurm_run.sh
+++ slurm_run.sh
@@ -2,12 +2,18 @@
 #SBATCH --ntasks=1                # number of processes to run
 #SBATCH --nodes=1                 # default number of nodes
 #SBATCH --partition=cpu_compute   # good enough for what I need
-#SBATCH --cpus-per-task=1         # for a multithredded job
+#SBATCH --cpus-per-task=3         # for a multithredded job
 # #SBATCH --mem=20g
-#SBATCH --job-name=array_5         # job name
-#SBATCH --output=outfiles/array_5_%J.txt   # output file
-#SBATCH --array=1-5
+
+# #SBATCH --job-name=collate                       # job name
+# #SBATCH --output=outfiles/collate_%J.txt         # output file
+
+#SBATCH --job-name=d3.8_pp40
+#SBATCH --output=outfiles/d3.8-%a.txt   # output file
+# #SBATCH --array=1-40
+
+# #SBATCH --dependency=afterany:13589
 
 module load R
 Rscript R/workflow.R $SLURM_ARRAY_TASK_ID 
-
+# Rscript R/collate_mcmc_output.R
