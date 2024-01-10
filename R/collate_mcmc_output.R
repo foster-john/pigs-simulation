00fe944fbf3c6ffb3240603628820dda15625a1f
JohnRFoster <john.foster@usda.gov>
2024-01-10 10:45:43 -0600

    update

--- R/collate_mcmc_output.R
+++ R/collate_mcmc_output.R
@@ -18,7 +18,7 @@ config_name <- "hpc_production"
 config <- config::get(config = config_name)
 
 start_density <- config$start_density
-start_density <- 1.475
+start_density <- 0.3
 
 top_dir <- config$top_dir
 out_dir <- config$out_dir
