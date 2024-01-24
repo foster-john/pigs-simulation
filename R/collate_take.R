a7829c7f8614a5a7870ee1dce947777c56b13720
JohnRFoster <fosterj@bu.edu>
2024-01-24 10:15:29 -0700

    effort summaries

--- R/collate_take.R
+++ R/collate_take.R
@@ -28,6 +28,9 @@ all_take <- tasks_ls$all_take
 path <- get_path("write", config_name, task_id)
 if(!dir.exists(path)) dir.create(path, recursive = TRUE, showWarnings = FALSE)
 
+write_rds(all_take, file.path(path, "all_take.rds"))
+
+
 take_summaries <- all_y |>
   group_by(simulation, p_id, start_density) |>
   my_summary() |>
@@ -50,6 +53,31 @@ write_rds(error_by_observation, file.path(path, "take_error_by_observation.rds")
 rm(error_by_observation)
 gc()
 message("\nposterior take error by observation done\n")
+
+take_effort_summary <- error_by_observation |>
+  group_by(property_id, PPNum, method) |>
+  summarise(
+    low_effort = quantile(effort_per, 0.025),
+    q1_effort = quantile(effort_per, 0.25),
+    median_effort = quantile(effort_per, 0.5),
+    mean_effort = mean(effort_per),
+    sum_effort = sum(effort_per),
+    q3_effort = quantile(effort_per, 0.75),
+    high_effort = quantile(effort_per, 0.975),
+    low_trap_count = quantile(trap_count, 0.025),
+    q1_trap_count = quantile(trap_count, 0.25),
+    median_trap_count = quantile(trap_count, 0.5),
+    mean_trap_count = mean(trap_count),
+    sum_trap_count = sum(trap_count),
+    q3_trap_count = quantile(trap_count, 0.75),
+    high_trap_count = quantile(trap_count, 0.975),
+    n_reps = n()
+  )
+
+write_rds(take_effort_summary, file.path(path, "take_effort_summary.rds"))
+rm(take_effort_summary)
+gc()
+message("\nposterior take error summary done\n")
 
 error_by_simulation <- all_y |>
   group_by(start_density, simulation) |>
