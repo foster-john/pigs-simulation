fc9347614c0eeeb746cf8322f512d40640cdaee5
JohnRFoster <fosterj@bu.edu>
2024-01-29 13:15:25 -0700

    subset the correct list

--- R/collate_take.R
+++ R/collate_take.R
@@ -35,14 +35,14 @@ take_summaries <- all_y
 write_rds(take_summaries, file.path(path, "take_summaries.rds"))
 rm(take_summaries)
 gc()
-message("\nposterior take error by observation done\n")
+message("\nposterior take summaries done\n")
 
-error_by_observation <- ls$all_by_observation
+error_by_observation <- tasks_ls$all_by_observation
 
 write_rds(error_by_observation, file.path(path, "take_error_by_observation.rds"))
 message("\nposterior take error by observation done\n")
 
-take_effort_summary <- ls$all_effort_summary
+take_effort_summary <- tasks_ls$all_effort_summary
 
 write_rds(take_effort_summary, file.path(path, "take_effort_summary.rds"))
 rm(take_effort_summary)
@@ -50,21 +50,21 @@ rm(error_by_observation)
 gc()
 message("\nposterior take error summary done\n")
 
-error_by_simulation <- ls$all_by_simulation
+error_by_simulation <- tasks_ls$all_by_simulation
 
 write_rds(error_by_simulation, file.path(path, "take_error_by_simulation.rds"))
 rm(error_by_simulation)
 gc()
 message("\nposterior take error by observation done\n")
 
-error_by_simulation_method <- ls$all_by_simulation_method
+error_by_simulation_method <- tasks_ls$all_by_simulation_method
 
 write_rds(error_by_simulation_method, file.path(path, "take_error_by_simulation_method.rds"))
 rm(error_by_simulation_method)
 gc()
 message("\nposterior take error by simulation method done\n")
 
-error_by_property <- ls$all_by_property
+error_by_property <- tasks_ls$all_by_property
 
 write_rds(error_by_property, file.path(path, "take_error_by_property.rds"))
 rm(error_by_property)
