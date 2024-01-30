74c8493bce60ff6ec58bbefa26b684a1a479986c
JohnRFoster <fosterj@bu.edu>
2024-01-30 10:49:05 -0700

    run glms

--- slurm_collate.sh
+++ slurm_collate.sh
@@ -3,13 +3,9 @@
 #SBATCH --nodes=1                 # default number of nodes
 #SBATCH --partition=cpu_compute   # good enough for what I need
 #SBATCH --cpus-per-task=1         # for a multithredded job
-#SBATCH --mem=28g
-#SBATCH --job-name=collate                       # job name
-#SBATCH --output=outfiles/collate_%a.txt         # output file
-#SBATCH --array=1-5
+#SBATCH --job-name=glm                       # job name
+#SBATCH --output=outfiles/glm.txt         # output file
 
 module load R
-Rscript R/collate_mcmc_parameters.R $SLURM_ARRAY_TASK_ID
-Rscript R/collate_abundance.R $SLURM_ARRAY_TASK_ID
-Rscript R/collate_take.R $SLURM_ARRAY_TASK_ID
+Rscript R/analysis_glm.R
 
