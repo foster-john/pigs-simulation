d4fa37795ac255c2200b3b43038fea45c7db484e
JohnRFoster <fosterj@bu.edu>
2024-01-31 12:21:49 -0700

    new workflow

--- R/analysis_glm.R
+++ R/analysis_glm.R
@@ -21,6 +21,8 @@ project_dir <- config$project_dir
 path <- file.path(top_dir, project_dir, analysis_dir, dev_dir, model_dir)
 density_dirs <- list.files(path)
 
+density_dirs <- grep("density_", density_dirs, value = TRUE)
+
 map_files <- function(dirs_vec, file_name, node){
 
   get_files <- function(density_dir, file_name, node){
@@ -82,9 +84,9 @@ abundance_summary <- map_files2(density_dirs, "abundance_summaries.rds") |>
   left_join(property_ids)
 
 a_join <- left_join(abundance_summary, abundance_error_by_observation)
-density <- a_join |> select(start_density, PPNum, contains("property"), contains("density"), obs_flag) |>
-  mutate(recovered = if_else(density >= low_density & density <= high_density, "Recovered", "Not Recovered"),
-         extinct = if_else(density == 0, "Extinct", "Extant"))
+density <- a_join |> select(start_density, PPNum, contains("property"), contains("abundance"), contains("density"), obs_flag) |>
+  mutate(recovered = if_else(abundance >= low_abundance & density <= high_abundance, "Recovered", "Not Recovered"),
+         extinct = if_else(abundance == 0, "Extinct", "Extant"))
 
 message("\nProportion recovered by extinct vs extant:")
 density |>
@@ -96,7 +98,8 @@ density |>
 f_name <- "all_take.rds"
 all_take <- map_files2(density_dirs, f_name) |>
   left_join(property_ids) |>
-  select(-order, -theta, -p, -property, -obs_flag, -simulation, -p_id)
+  select(-theta, -p, -property, -obs_flag, -simulation, -p_id) |>
+  mutate(effort = effort_per * trap_count)
 
 sum_take <- all_take |>
   select(property_id, PPNum, sum_take) |>
@@ -116,18 +119,24 @@ all_methods <- map_files2(density_dirs, f_name)  |>
                values_to = "parameter_value") |>
   filter(!is.na(parameter_value))
 
-f_name <- "take_effort_summary.rds"
-take_effort_summary <- map_files2(density_dirs, f_name) |>
-  left_join(property_ids) |>
-  ungroup()
+take_effort_summary <- all_take |>
+  group_by(property_id, PPNum, method) |>
+  summarise(mean_effort_per = mean(effort_per),
+            sum_effort_per = sum(effort_per),
+            mean_effort = mean(effort),
+            sum_effort = sum(effort),
+            mean_trap_count = mean(trap_count),
+            sum_trap_count = sum(trap_count),
+            n_reps = n())
 
 density_obs_take <- left_join(take_effort_summary, density_obs)
 density_obs_take_params <- left_join(density_obs_take, all_methods, relationship = "many-to-many")
 
-n_methods_prop <- all_take |>
+n_methods_pp <- all_take |>
   select(property_id, PPNum, method) |>
   distinct() |>
-  count(property_id) |>
+  group_by(property_id, PPNum) |>
+  count() |>
   rename(n_methods_used = n)
 
 n_return <- all_take |>
@@ -143,101 +152,252 @@ n_return <- all_take |>
   mutate(return_interval = c(0, diff(PPNum))) |>
   ungroup() |>
   rename(methods_used = method) |>
-  left_join(n_methods_prop)
+  left_join(n_methods_pp)
 
-data_final_join <- left_join(density_obs_take_params, n_return)
+data_final_join <- left_join(density_obs_take, n_return)
 
 rescale_variable <- function(x) (x - mean(x)) / sd(x)
 
 data <- data_final_join |>
-  filter(density > 0) |>
-  pivot_wider(names_from = parameter_name,
-              values_from = parameter_value) |>
+  ungroup() |>
+  filter(density > 0,
+         nm_rmse_abundance < quantile(data$nm_rmse_abundance, 0.999)) |>
+  rename(nrmse = nm_rmse_abundance) |>
+  select(-abundance, -nm_rmse_density, -rmse_abundance, -rmse_density) |>
   mutate(method = as.factor(method),
-         methods_used = as.factor(methods_used)) |>
+         methods_used = as.factor(methods_used),
+         property_id = as.factor(property_id)) |>
   group_by(method) |>
-  mutate(mean_effort = rescale_variable(mean_effort),
+  mutate(mean_effort_method = rescale_variable(mean_effort),
+         sum_effort_method = rescale_variable(sum_effort),
+         mean_effort_per_method = rescale_variable(mean_effort_per),
+         sum_effort_per_method = rescale_variable(sum_effort_per),
+         mean_trap_count_method = rescale_variable(mean_trap_count),
+         sum_trap_count_method = rescale_variable(sum_trap_count),
+         n_reps_method = rescale_variable(n_reps)) |>
+  ungroup() |>
+  # group_by(methods_used) |>
+  # mutate() |>
+  # ungroup() |>
+  mutate(med_density = rescale_variable(med_density),
+         return_interval = rescale_variable(return_interval),
+         #n_methods_used = rescale_variable(n_methods_used),
+         sum_take_density = rescale_variable(sum_take_density),
+         property_area = rescale_variable(property_area),
+         start_density = as.factor(start_density),
+         mean_effort = rescale_variable(mean_effort),
          sum_effort = rescale_variable(sum_effort),
+         mean_effort_per = rescale_variable(mean_effort_per),
+         sum_effort_per = rescale_variable(sum_effort_per),
          mean_trap_count = rescale_variable(mean_trap_count),
          sum_trap_count = rescale_variable(sum_trap_count),
-         n_reps = rescale_variable(n_reps)) |>
-  ungroup() |>
-  group_by(methods_used) |>
-  mutate(return_interval = rescale_variable(return_interval)) |>
-  ungroup() |>
-  mutate(med_density = rescale_variable(med_density),
-         n_methods_used = rescale_variable(n_methods_used),
-         sum_take_density = rescale_variable(sum_take_density),
-         property_area = rescale_variable(property_area))
+         n_reps = rescale_variable(n_reps))
 
 path <- file.path(top_dir, project_dir, analysis_dir, dev_dir, "GLMs", model_dir)
 if(!dir.exists(path)) dir.create(path, recursive = TRUE, showWarnings = FALSE)
 
 
-## glm to explain normalised rmse given mean effort
-p <- 1.01
-
 args <- commandArgs(trailingOnly = TRUE)
 task_id <- as.numeric(args[1])
 
-if(task_id == 1){
-  message("Fit full model glm")
-  mFull <- glmer(nm_rmse_density ~                         # global intercept
-                   (1 | method) +                          # random effect intercept for each method
-                   (1 | methods_used) +                    # random effect intercept for methods used in each PP
-                   (0 + mean_effort | method) +            # mean effort per trap deviations across methods
-                   (0 + sum_effort | method) +             # total effort per trap deviations across methods
-                   (0 + mean_trap_count | method) +        # mean number of traps deviations across methods
-                   (0 + sum_trap_count | method) +         # total number of traps deviations across methods
-                   (0 + n_reps | method) +                 # number events deviations across methods
-                   (0 + return_interval | methods_used) +  # return interval deviations across levels of methods used
-                   med_density +                           # the effect (slope) of median estimated density
-                   n_methods_used +                        # the effect (slope) of number of methods used in PP
-                   sum_take_density +                      # the effect (slope) of total take in PP as a density
-                   property_area +                         # the effect (slope) of property area
-                   I(med_density * n_methods_used) +       # the interaction between median estimated density and number of methods used in PP
-                   I(med_density * sum_take_density) +     # the interaction between median estimated density and total take in PP as a density
-                   I(med_density * property_area) +        # the interaction between median estimated density and property area
-                   I(n_methods_used * sum_take_density) +  # the interaction between number of methods used in PP and total take in PP as a density
-                   I(n_methods_used * property_area) +     # the interaction between number of methods used in PP property area
-                   I(sum_take_density * property_area),    # the interaction between total take in PP as a density and property area
-                 family = Tweedie(p = p),
-                 data = data)
 
-  summary(mFull)
-  write_rds(mFull, file.path(path, "glmer_full.rds"))
+if(task_id == 0){
+
+  ## Null model - random intercepts by methods used in PP
+  message("\n=== Model 0 ===")
+  message("  GLM")
+  m_list <- list()
+  glm <- glmer(nrmse ~
+                   (1 | methods_used),
+                 family = Gamma(link = "log"),
+                 data = data)
+  m_list$glm <- glm
+  write_rds(m_list, file.path(path, paste0("m", task_id, ".rds")))
+  message("    -> done")
+
+  message("  GAM")
+  gam <- gam(nrmse ~
+                 s(methods_used, bs = "re"),
+               family = Gamma(link = "log"),
+               data = data)
+  m_list$m0 <- gam
+  write_rds(m_list, file.path(path, paste0("m", task_id, ".rds")))
+  message("    -> done")
+
+} else if(task_id == 1){
+
+  ## All individual effects +
+  ## mean effort and mean trap count in a PP across methods
+  message("\n=== Model 1 ===")
+  message("  GLM")
+  m_list <- list()
+  glm <- glmer(nrmse ~
+                   (1 | methods_used) +
+                   return_interval +
+                   med_density +
+                   delta +
+                   n_reps +
+                   n_methods_used +
+                   sum_take_density +
+                   property_area +
+                   mean_effort +
+                   mean_trap_count,
+                 family = Gamma(link = "log"),
+                 data = data)
+  m_list$glm <- glm
+  write_rds(m_list, file.path(path, paste0("m", task_id, ".rds")))
+  message("    -> done")
+
+  message("  GAM")
+  gam <- gam(nrmse ~
+                 s(methods_used, bs = "re") +
+                 s(return_interval, bs = "cr") +
+                 s(med_density, bs = "cr") +
+                 s(delta, bs = "cr") +
+                 s(n_reps, bs = "cr") +
+                 s(n_methods_used, bs = "cr") +
+                 s(sum_take_density, bs = "cr") +
+                 s(property_area, bs = "cr") +
+                 s(mean_effort, bs = "cr") +
+                 s(mean_trap_count, bs = "cr"),
+               family = Gamma(link = "log"),
+               data = data)
+  m_list$glm <- gam
+  write_rds(m_list, file.path(path, paste0("m", task_id, ".rds")))
+  message("    -> done")
 
 } else if(task_id == 2){
-  message("Fit full model gam")
-  mFullg <- gam(nm_rmse_density ~                         # global intercept
-                  s(method, bs = "re") +                          # random effect intercept for each method
-                  s(methods_used, bs = "re") +                    # random effect intercept for methods used in each PP
-                  s(mean_effort, by = method) +            # mean effort per trap deviations across methods
-                  s(sum_effort, by = method) +             # total effort per trap deviations across methods
-                  s(mean_trap_count, by = method) +        # mean number of traps deviations across methods
-                  s(sum_trap_count, by = method) +         # total number of traps deviations across methods
-                  s(n_reps, by = method) +                 # number events deviations across methods
-                  s(return_interval, by = methods_used) +  # return interval deviations across levels of methods used
-                  s(med_density, bs = "cs") +                           # the effect (slope) of median estimated density
-                  s(n_methods_used, bs = "cs") +                        # the effect (slope) of number of methods used in PP
-                  s(sum_take_density, bs = "cs") +                      # the effect (slope) of total take in PP as a density
-                  s(property_area, bs = "cs") +                         # the effect (slope) of property area
-                  s(med_density, n_methods_used) +       # the interaction between median estimated density and number of methods used in PP
-                  s(med_density, sum_take_density) +     # the interaction between median estimated density and total take in PP as a density
-                  s(med_density, property_area) +        # the interaction between median estimated density and property area
-                  s(n_methods_used, sum_take_density) +  # the interaction between number of methods used in PP and total take in PP as a density
-                  s(n_methods_used, property_area) +     # the interaction between number of methods used in PP property area
-                  s(sum_take_density, property_area),    # the interaction between total take in PP as a density and property area
-                family = Tweedie(p = p),
-                data = data)
 
-  summary(mFullg)
-  write_rds(mFullg, file.path(path, "gam_full.rds"))
+  ## All individual effects +
+  ## sum effort and sum trap count in a PP across methods
+  message("\n=== Model 2 ===")
+  message("  GLM")
+  m_list <- list()
+  glm <- glmer(nrmse ~
+                 (1 | methods_used) +
+                 return_interval +
+                 med_density +
+                 delta +
+                 n_reps +
+                 n_methods_used +
+                 sum_take_density +
+                 property_area +
+                 sum_effort +
+                 sum_trap_count,
+               family = Gamma(link = "log"),
+               data = data)
+  m_list$glm <- glm
+  write_rds(m_list, file.path(path, paste0("m", task_id, ".rds")))
+  message("    -> done")
+
+  message("  GAM")
+  gam <- gam(nrmse ~
+               s(methods_used, bs = "re") +
+               s(return_interval, bs = "cr") +
+               s(med_density, bs = "cr") +
+               s(delta, bs = "cr") +
+               s(n_reps, bs = "cr") +
+               s(n_methods_used, bs = "cr") +
+               s(sum_take_density, bs = "cr") +
+               s(property_area, bs = "cr") +
+               s(sum_effort, bs = "cr") +
+               s(sum_trap_count, bs = "cr"),
+             family = Gamma(link = "log"),
+             data = data)
+  m_list$glm <- gam
+  write_rds(m_list, file.path(path, paste0("m", task_id, ".rds")))
+  message("    -> done")
+
+} else if(task_id == 3){
+
+  ## All individual effects +
+  ## mean effort and mean trap count in a PP for each method +
+  ## random intercept by method
+  message("\n=== Model 3 ===")
+  message("  GLM")
+  m_list <- list()
+  glm <- glmer(nrmse ~
+                 (1 | methods_used) +
+                 (1 | method) +
+                 return_interval +
+                 med_density +
+                 delta +
+                 n_reps +
+                 n_methods_used +
+                 sum_take_density +
+                 property_area +
+                 mean_effort_method +
+                 mean_trap_count_method,
+               family = Gamma(link = "log"),
+               data = data)
+  m_list$glm <- glm
+  write_rds(m_list, file.path(path, paste0("m", task_id, ".rds")))
+  message("    -> done")
+
+  message("  GAM")
+  gam <- gam(nrmse ~
+               s(methods_used, bs = "re") +
+               s(method, bs = "re") +
+               s(return_interval, bs = "cr") +
+               s(med_density, bs = "cr") +
+               s(delta, bs = "cr") +
+               s(n_reps, bs = "cr") +
+               s(n_methods_used, bs = "cr") +
+               s(sum_take_density, bs = "cr") +
+               s(property_area, bs = "cr") +
+               s(mean_effort_method, bs = "cr") +
+               s(mean_trap_count_method, bs = "cr"),
+             family = Gamma(link = "log"),
+             data = data)
+  m_list$glm <- gam
+  write_rds(m_list, file.path(path, paste0("m", task_id, ".rds")))
+  message("    -> done")
+
+} else if(task_id == 4){
+
+  ## All individual effects +
+  ## sum effort and sum trap count in a PP for each method +
+  ## random intercept by method
+  message("\n=== Model 4 ===")
+  message("  GLM")
+  m_list <- list()
+  glm <- glmer(nrmse ~
+                 (1 | methods_used) +
+                 (1 | method) +
+                 return_interval +
+                 med_density +
+                 delta +
+                 n_reps +
+                 n_methods_used +
+                 sum_take_density +
+                 property_area +
+                 sum_effort_method +
+                 sum_trap_count_method,
+               family = Gamma(link = "log"),
+               data = data)
+  m_list$glm <- glm
+  write_rds(m_list, file.path(path, paste0("m", task_id, ".rds")))
+  message("    -> done")
+
+  message("  GAM")
+  gam <- gam(nrmse ~
+               s(methods_used, bs = "re") +
+               s(method, bs = "re") +
+               s(return_interval, bs = "cr") +
+               s(med_density, bs = "cr") +
+               s(delta, bs = "cr") +
+               s(n_reps, bs = "cr") +
+               s(n_methods_used, bs = "cr") +
+               s(sum_take_density, bs = "cr") +
+               s(property_area, bs = "cr") +
+               s(sum_effort_method, bs = "cr") +
+               s(sum_trap_count_method, bs = "cr"),
+             family = Gamma(link = "log"),
+             data = data)
+  m_list$glm <- gam
+  write_rds(m_list, file.path(path, paste0("m", task_id, ".rds")))
+  message("    -> done")
 
 }
-
-
-
-
 
 
