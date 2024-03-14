6880e39884ecd91748d8e6fdee65443cff2c7843
JohnRFoster <fosterj@bu.edu>
2024-03-14 11:57:39 -0600

    bigger hyper grid
    Increasing the parameter space for the hyper grid search, previous runs were on the boundary for min child depth, and I want to see what happens with chld weight and colsample.

--- R/analysis_model_workflow.R
+++ R/analysis_model_workflow.R
@@ -23,20 +23,19 @@ project_dir <- config$project_dir
 path <- file.path(top_dir, project_dir, analysis_dir, dev_dir, model_dir)
 
 data <- read_rds(file.path(path, "abundanceScoresByPrimaryPeriod.rds")) |>
-  filter(density > 0) |>
   ungroup() |>
-  group_by()
+  filter(density > 0)
 
 responses <- c("nm_rmse_density", "mpe_density", "mbias_density")
 
 # hyperparameter grid
 hyper_grid <- expand_grid(
   responses = responses,
-  eta = c(0.05),
-  max_depth = 3:5,
-  min_child_weight = 1,
+  eta = 0.05,
+  max_depth = 3:8,
+  min_child_weight = c(0.5, 1, 1.5),
   subsample = 0.5,
-  colsample_bytree = 0.5,
+  colsample_bytree = c(0.5, 0.75, 1),
   gamma = c(0, 1, 10, 100, 1000),
   lambda = c(0, 1e-2, 0.1, 1, 100, 1000, 10000),
   alpha = c(0, 1e-2, 0.1, 1, 100, 1000, 10000),
@@ -50,11 +49,12 @@ n_by_response <- hyper_grid |>
   pull(n) |>
   unique()
 
-n_models_per_array <- 35
+n_models_per_array <- 210
 
 array_nums_1 <- rep(seq(1, ceiling(n_by_response / n_models_per_array)), each = n_models_per_array)
 array_nums_2 <- array_nums_1 + max(array_nums_1)
 array_nums_3 <- array_nums_1 + max(array_nums_1)*2
+max(array_nums_3)
 
 hyper_grid <- hyper_grid |>
   mutate(array = c(array_nums_1, array_nums_2, array_nums_3))
@@ -93,7 +93,11 @@ start_time <- Sys.time()
 
 # grid search
 for(i in seq_len(nrow(array_grid))) {
-  message("[", i, "/", nrow(array_grid), "]")
+
+  if(i %% 10 == 0){
+    message("[", i, "/", nrow(array_grid), "] ", round(i/nrow(array_grid)*100), "%")
+  }
+
   set.seed(123)
   m <- xgb.cv(
     data = X,
