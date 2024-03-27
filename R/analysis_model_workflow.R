e7c23af3cf5bb6bfeb9e9804fff4e72aa1aafe7e
JohnRFoster <fosterj@bu.edu>
2024-03-27 17:03:58 -0600

    benchmark with omp_num_threads

--- R/analysis_model_workflow.R
+++ R/analysis_model_workflow.R
@@ -26,24 +26,122 @@ path <- file.path(top_dir, project_dir, analysis_dir, dev_dir, model_dir)
 data <- read_rds(file.path(path, "abundanceScoresByPrimaryPeriod.rds")) |>
   ungroup() |>
   filter(density > 0) |>
-  mutate(mbias_density_class = as.numeric(mbias_density > 0))
+  mutate(mbias_density_class = as.numeric(mbias_density > 0)) |>
+  rename(mbias_density_reg = mbias_density)
 
 responses <- c("nm_rmse_density", "mpe_density", "mbias_density_reg", "mbias_density_class")
+
+eta_grid <- tibble(
+  responses = responses,
+  eta = c(0.1, 0.1, 0.3, 0.1))
+
+args <- commandArgs(trailingOnly = TRUE)
+task_id <- as.numeric(args[1])
+if(is.na(task_id)) task_id <- 1
+message("task id: ", task_id)
+
+task <- eta_grid |>
+  dplyr::slice(task_id)
+
+y <- task$responses
+message("\ny: ", y)
+
+eta <- task$eta
+message("eta: ", eta)
+
+df_model <- subset_rename(data, y)
+baked_data <- my_recipe(df_model$train, df_model$test)
+
+train_data <- baked_data$df_train
+X <- train_data |>
+  select(-y) |>
+  as.matrix()
+Y <- train_data |> pull(y)
+
+objective <- if_else(y == "mbias_density_class", "binary:logistic", "reg:squarederror")
+
+fit_xgBoost <- function(eta, X, Y, objective, omp_thread = Sys.getenv("OMP_NUM_THREADS")){
+  m <- xgb.cv(
+    data = X,
+    label = Y,
+    nrounds = 5000,
+    objective = objective,
+    metrics = "rmse",
+    early_stopping_rounds = 50,
+    nfold = 10,
+    nthread = as.numeric(omp_thread),
+    verbose = 0,
+    params = list(
+      eta = eta
+    )
+  )
+}
+
+timing <- system.time(
+  fit_xgBoost(eta, X, Y, objective)
+)[3]
+
+timing
+
+
+stop()
+
 
 # hyperparameter grid
 hyper_grid <- expand_grid(
   responses = responses,
-  eta = 0.05,
   max_depth = 3:8,
-  min_child_weight = c(0.5, 1, 1.5),
-  subsample = 0.5,
+  min_child_weight = c(0.5, 1),
+  subsample = c(0.5, 1),
   colsample_bytree = c(0.5, 0.75, 1),
   gamma = c(0, 1, 10, 100, 1000),
-  lambda = c(0, 1e-2, 0.1, 1, 100, 1000, 10000),
-  alpha = c(0, 1e-2, 0.1, 1, 100, 1000, 10000),
+  lambda = c(0, 1e-2, 0.1, 1, 100, 1000),
+  alpha = c(0, 1e-2, 0.1, 1, 100, 1000),
   rmse = 0,
   trees = 0
 )
+
+# grid search
+start_time <- Sys.time()
+# for(i in seq_len(nrow(array_grid))) {
+for(i in 11:20) {
+
+  if(i %% 2 == 0){
+    message("[", i, "/", nrow(array_grid), "] ", round(i/nrow(array_grid)*100), "%")
+  }
+
+
+
+  model_time <- Sys.time()
+  set.seed(123)
+  m <- xgb.cv(
+    data = X,
+    label = Y,
+    nrounds = 5000,
+    objective = objective,
+    metrics = "rmse",
+    early_stopping_rounds = 50,
+    nfold = 10,
+    nthread = 1,
+    verbose = 0,
+    params = list(
+      eta = array_grid$eta[i]
+    )
+  )
+  array_grid$rmse[i] <- min(m$evaluation_log$test_rmse_mean)
+  array_grid$trees[i] <- m$best_iteration
+
+  total_time <- Sys.time() - model_time
+  message("Model time: ")
+  print(total_time)
+
+}
+
+
+
+
+
+
 
 n_by_response <- hyper_grid |>
   group_by(responses) |>
